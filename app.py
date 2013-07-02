b3e84607322d9d674c099d5f5888abee307e5295
Fox Wilson <2016fwilson@tjhsst.edu>
2013-07-02 14:29:42 -0400

    content for templates

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, render_template
+from random import choice
+## Helpers
+def get_word():
+    words = [
+            "potato",
+            "the",
+            "it",
+            "Bruce"
+            ]
+    return choice(words)
 app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template('index.html')
+    return render_template('index.html', word=get_word())
 
 @app.route('/about/')
 def about():
@@ -13,5 +23,9 @@ def about():
 def results():
     return render_template('results.html')
 
+@app.route('/feedback/')
+def feedback():
+    return render_template('feedback.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
